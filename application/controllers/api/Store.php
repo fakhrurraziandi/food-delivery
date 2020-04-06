32729054cdccc97dfe64b06d417cef79a8a642c4
fakhrurraziandi <fakhrurrazi.andi@gmail.com>
2020-04-07 04:37:33 +0700

    update api store

--- application/controllers/api/Store.php
+++ application/controllers/api/Store.php
@@ -9,9 +9,16 @@ class Store extends AuthenticatedRestController{
 
         $current_lat = $this->input->get('current_lat');
         $current_lng = $this->input->get('current_lng');
+        $radius = 10; // km
         $keyword = $this->input->get('keyword');
 
         $sql = "SELECT * FROM stores";
+        
+        /* 
+        TODO
+        -- Query for keyword search
+        -- Query for location search with radius
+         */
 
         $query = $this->db->query($sql);
         $stores = $query->result();
